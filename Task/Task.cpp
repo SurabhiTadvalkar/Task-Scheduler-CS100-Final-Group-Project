22f93315353b962b71d5d5390f419025bc5fe38d
ctruo <ctruongg67@gmail.com>
2021-05-17 16:13:07 -0700

    implemented and tested Task class (leaf)

---
+++ Task/Task.cpp
@@ -0,0 +1,74 @@
+#include "Task.hpp"
+
+Task::Task()
+{
+    this->name = "";
+    this->description = "";
+    this-> deadline = "";
+    this->status = false;
+}
+
+
+void Task::print() {
+    if (!name.empty() )   //tasks need a name
+    {
+        cout << "Task: " << name << endl;
+    
+        if (!description.empty() ) 
+        {
+            cout << "Description: " << description << endl;
+        }
+        if (!deadline.empty() ) 
+        {
+            cout << "Deadline: " << deadline << endl;
+        }
+        if (status == false) 
+        {
+            cout << "Status: NOT FINISHED" << endl;
+        }
+        else if (status == true) 
+        {
+            cout << "Status: FINISHED" << endl;
+        }
+    }
+}
+
+void Task::addName(string _name)
+{
+    this->name = _name;
+}
+
+void Task::addDescription(string _description)
+{
+    this->description = _description;
+}
+
+void Task::addDeadline(string _deadline)
+{
+    this->deadline = _deadline;
+}
+
+void Task::setStatus(bool _status)
+{
+    this->status = _status;
+}
+
+string Task::getName()
+{
+    return this->name;
+}
+
+string Task::getDescription()
+{
+    return this->description;
+}
+
+string Task::getDeadline()
+{
+    return this->deadline;
+}
+
+bool Task::getStatus()
+{
+    return this->status;
+}
