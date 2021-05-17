b1f8c9f2526fc9089c5f79ba9a6dfde9b27e46ab
ctruo <ctruongg67@gmail.com>
2021-05-17 16:06:36 -0700

    implemented Tasks class (component)

---
+++ Tasks/Tasks.hpp
@@ -0,0 +1,31 @@
+#ifndef __TASKS_HPP__
+#define __TASKS_HPP__
+
+#include <string>
+using std::string;
+
+class Tasks {
+    protected: 
+        string name;
+        string description;
+        string deadline;
+        bool status;
+
+    public:
+        Tasks();
+
+        ~Tasks();
+
+        virtual void print() = 0;
+        virtual void addName(string _name) = 0;
+        virtual void addDescription(string _description) = 0;
+        virtual void addDeadline(string _deadline) = 0;
+        virtual void setStatus(bool _status) = 0;
+
+        virtual string getName() = 0;
+        virtual string getDescription() = 0;
+        virtual string getDeadline() = 0;
+        virtual bool getStatus() = 0;
+};
+
+#endif
