0bc83f98b790be362b979ba16285807c592c5ac6
SurabhiTadvalkar <surabhi.tadvalkar@gmail.com>
2021-06-01 06:50:37 -0700

    extra testing for strategy

--- Strategysrc/Scheduler.cpp
+++ Strategysrc/Scheduler.cpp
@@ -5,21 +5,21 @@
 using std::cout;
 using std::endl;
 Scheduler::Scheduler(Tasks* t2) : Prioritize(t2){ }
- Tasks* Scheduler::sort(){ 
+Tasks* Scheduler::sort(){
                         if(typeid(Task)==typeid(*myTask)){return myTask;}
                         if (typeid(Project) == typeid(*myTask)){
                                 Project* tasks = dynamic_cast<Project*>(myTask);
                                 vector<Tasks*>* v = tasks->getTasks();
                                 if(v->size() ==0){return tasks;}
-                                if(v->size() ==1){ 
-                                        if(typeid(Project) == typeid(*v->at(0))){ 
+                                if(v->size() ==1){
+                                        if(typeid(Project) == typeid(*v->at(0))){
                                                 Tasks* temp = v->at(0);
-						v->at(0) = (new Scheduler(temp))->sort(); 
-						v->at(0)->setParent(tasks);
+                                                v->at(0) = (new Scheduler(temp))->sort();
+                                                v->at(0)->setParent(tasks);
                                         }
-					return tasks;
+                                        return tasks;
                                 }
-				
+
                                 string key = "";
                                 for(int i=1; i<v->size(); ++i){
                                         if(typeid(Project) == typeid(*v->at(i))){
@@ -29,7 +29,7 @@ Scheduler::Scheduler(Tasks* t2) : Prioritize(t2){ }
                                         }
                                         else {key = v->at(i)->getDeadline();}
 
-                                        for(int j = i-1; j>=0; --j){ 
+                                        for(int j = i-1; j>=0; --j){
                                                 if(tasks->isLessThan(v->at(i)->getDeadline(),key )){
                                                         Tasks* swap = v->at(j+1);
                                                         v->at(j+1) = v->at(j);
@@ -39,10 +39,8 @@ Scheduler::Scheduler(Tasks* t2) : Prioritize(t2){ }
                                         }
 
                                 }
-				return tasks;
+                                return tasks;
                         }
                         return myTask;
-
-                }
-
+}
 
